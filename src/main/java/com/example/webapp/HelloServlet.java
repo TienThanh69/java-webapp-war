03518c08f868656eb66f29281d44501da8551e99
TienThanh69 <apksuyuki1@gmail.com>
2025-06-23 16:25:06 +0700

    Khởi tạo project Maven WAR

---
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.example.webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello from HelloServlet!</h1>");
+    }
+}
